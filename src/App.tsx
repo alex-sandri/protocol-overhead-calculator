229e108855437a9e165c8da6c47a5ae8234e23fe
Alex Sandri <me@alexsandri.com>
2020-09-28 19:14:42 +0200

    Show protocol fields

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import './App.css';
 interface Protocol
 {
   name: string,
+  fields: {
+    name: string,
+    size: number,
+  }[],
 }
 
 class App extends React.Component
@@ -11,7 +15,17 @@ class App extends React.Component
   private protocols: Protocol[] = [
     {
       name: "Ethernet",
+      fields: [
+        { name: "Preamble", size: 7, },
+        { name: "Start Frame Delimiter", size: 1, },
+        { name: "Destination MAC Address", size: 6, },
+        { name: "Source MAC Address", size: 6, },
+        { name: "EtherType", size: 2, },
+        { name: "Data", size: 7, },
+        { name: "Frame Check Sequence", size: 7, },
+      ]
     },
+    /*
     {
       name: "IPv4",
     },
@@ -24,6 +38,7 @@ class App extends React.Component
     {
       name: "UDP",
     },
+    */
   ]
 
   state = {
@@ -42,7 +57,16 @@ class App extends React.Component
               (<button key={protocol.name} onClick={() => this.setState({ protocol })} className="ProtocolListTile">{protocol.name}</button>))
           }
         </ul>
-        <div>{this.state.protocol.name}</div>
+        <div className="Protocol">
+          <h1 className="ProtocolTitle">{this.state.protocol.name}</h1>
+          {
+            this.state.protocol.fields?.map((field) =>
+              (<div key={field.name} className="ProtocolField">
+                <p className="ProtocolFieldName">{field.name}</p>
+                <p className="ProtocolFieldSize">{field.size}</p>
+              </div>))
+          }
+        </div>
       </div>
     );
   }
