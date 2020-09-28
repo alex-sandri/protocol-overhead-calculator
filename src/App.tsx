650c4bc8206f008afa4c8900b1dbb32f629b0fde
Alex Sandri <me@alexsandri.com>
2020-09-28 16:26:33 +0200

    Show selected protocol

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Protocol Overhead Calculator</h1>
-      </header>
-      <ul className="ProtocolsListView">
-        {
-          [ "Ethernet", "IPv4", "IPv6", "TCP", "UDP" ].map((protocol, index) =>
-            (<button key={protocol} className="ProtocolListTile">{protocol}</button>))
-        }
-      </ul>
-    </div>
-  );
+class App extends React.Component
+{
+  state = {
+    protocol: "",
+  }
+
+  private showProtocol(protocol: string) {
+    this.setState({ protocol })
+
+    switch (protocol)
+    {
+      case "Ethernet":
+        break;
+      case "IPv4":
+        break;
+      case "IPv6":
+        break;
+      case "TCP":
+        break;
+      case "UDP":
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Protocol Overhead Calculator</h1>
+        </header>
+        <ul className="ProtocolsListView">
+          {
+            [ "Ethernet", "IPv4", "IPv6", "TCP", "UDP" ].map((protocol) =>
+              (<button key={protocol} onClick={() => this.showProtocol(protocol)} className="ProtocolListTile">{protocol}</button>))
+          }
+        </ul>
+        <div>{this.state.protocol}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
